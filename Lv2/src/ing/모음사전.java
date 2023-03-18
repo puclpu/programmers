f7c48c89f87c1e10fd835586d97744a5b643e0ff
kimchaerin <puclpu18@gmail.com>
2023-03-18 23:18:56 +0900

    모음사전 > ing

---
+++ Lv2/src/ing/모음사전.java
@@ -0,0 +1,16 @@
+package ing;
+
+public class 모음사전 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class AeiouDictionary {
+    public int solution(String word) {
+        int answer = 0;
+        return answer;
+    }
+}
