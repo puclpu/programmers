44526ddf07d561a4c7d677a6c1e70d5db47ba985
kimchaerin <puclpu18@gmail.com>
2023-03-26 22:49:22 +0900

    가장큰수 > ing

---
+++ Lv2/src/ing/가장큰수.java
@@ -0,0 +1,23 @@
+package ing;
+
+import java.util.LinkedList;
+
+public class 가장큰수 {
+
+	public static void main(String[] args) {
+		int[] numbers = {};
+		BiggestNumber bn = new BiggestNumber();
+		String result = bn.solution(numbers);
+		System.out.println(result);
+	}
+
+}
+class BiggestNumber {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        return answer;
+    }
+}
