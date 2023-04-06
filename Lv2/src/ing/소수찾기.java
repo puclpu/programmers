9ceea3b200581609b9bade7b7efa0b4236b88060
kimchaerin <puclpu18@gmail.com>
2023-04-06 23:05:56 +0900

    소수찾기 > ing

--- Lv2/src/ing/소수찾기.java
+++ Lv2/src/ing/소수찾기.java
@@ -1,11 +1,11 @@
 package ing;
 
+
 public class 소수찾기 {
 
 	public static void main(String[] args) {
-		FindPrimeNumber fpn = new FindPrimeNumber();
-		int result = fpn.solution("17");
-		System.out.println(result);
+		
+		
 	}
 
 }
